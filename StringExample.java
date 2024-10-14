cc6c400d8c01fc536102911f2eb13ad9a2e4f292
horokkk <smurfystrawberry@gmail.com>
2024-10-14 17:21:01 +0900

    Create StringExample.java

---
+++ StringExample.java
@@ -0,0 +1,18 @@
+public class StringExample {
+    public static void main(String[] args){
+        String name = "김지윤";
+        String job = "감자 학생";
+        System.out.println(name);
+        System.out.println(job);
+
+        String str = "나는 \"자바\"를 배우고 있습니다.";
+        System.out.println(str);
+
+        str = "번호\t이름\t직업";
+        System.out.println(str);
+
+        System.out.print("나는\n");
+        System.out.print("자바를\n");
+        System.out.print("배우고 있습니다.");
+    }
+}
